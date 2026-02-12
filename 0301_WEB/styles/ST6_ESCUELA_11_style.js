ecbdddcc28c80f8e2594a6205756a85915e87f41
De1tha <israelleoncansino@gmail.com>
2026-02-12 12:32:51 -0600

    Add files via upload

---
+++ 0301_WEB/styles/ST6_ESCUELA_11_style.js
@@ -0,0 +1,47 @@
+var size = 0;
+var placement = 'point';
+
+var style_ST6_ESCUELA_11 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("var value = '';");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#ffffff";
+    var bufferColor = "#2511b1";
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("nombre1") !== null) {
+        labelText = String(feature.get("nombre1"));
+    }
+    
+        function rules_ST6_ESCUELA_11(feature, value) {
+            var context = {
+                feature: feature,
+                variables: {}
+            };
+            // Start of if blocks and style check logic
+            if (exp_ST6_ESCUELA_11rule0_eval_expression(context)) {
+                      return [ new ol.style.Style({
+        image: new ol.style.Circle({radius: 10.0 + size,
+            displacement: [0, 0], stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 3.8}), fill: new ol.style.Fill({color: 'rgba(164,113,88,1.0)'})}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    }
+            else {
+                return [];
+            }
+        }
+        var style = rules_ST6_ESCUELA_11(feature, value);
+        ;
+
+    return style;
+};
